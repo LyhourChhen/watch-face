cb07dd33f01d50ee374dc325309036841b9bd6d0
LyhourChhen <ukamilove66@gmail.com>
2020-10-15 17:23:11 +0700

    update layout

--- lib/layouts/face.dart
+++ lib/layouts/face.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class Face extends StatefulWidget {
   @override
@@ -9,17 +10,36 @@ class _FaceState extends State<Face> {
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
-    final screenHeight = screenSize.height;
-    final screenWidth = screenSize.width;
+    // final screenHeight = screenSize.height;
+    // final screenWidth = screenSize.width;
     return Material(
       child: Scaffold(
         body: Container(
-          width: screenWidth,
-          height: screenHeight,
-          child: Text(
-            "AsurRaa",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
+          // width: screenWidth,
+          // height: screenHeight,
+          child: Center(
+              child: Container(
+            child: Column(
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  child: Image.asset('assets/logo.png'),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 5),
+                  child: Container(child: Lottie.asset('assets/loading.json')),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 5),
+                  child: Text(
+                    "Powered by AsurRaa",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          )),
         ),
       ),
     );
